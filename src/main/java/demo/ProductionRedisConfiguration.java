5f1a60d6cd5b71d5ed1c4ac4cdbbccdab3f235c8
Zoltan Altfatter <altfatterz@gmail.com>
2015-02-09 14:00:43 +0100

    remove unncessary logging

--- src/main/java/demo/ProductionRedisConfiguration.java
+++ src/main/java/demo/ProductionRedisConfiguration.java
@@ -1,7 +1,5 @@
 package demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,20 +13,12 @@ import java.net.URISyntaxException;
 @Configuration
 public class ProductionRedisConfiguration {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProductionRedisConfiguration.class);
     @Bean
     public JedisConnectionFactory connectionFactory() throws URISyntaxException {
         JedisConnectionFactory redis = new JedisConnectionFactory();
         String redisUrl = System.getenv("REDISCLOUD_URL");
 
-        LOGGER.info("REDISCLOUD_URL:" + redisUrl);
-
         URI redisUri = new URI(redisUrl);
-
-        LOGGER.info("hostname:" + redisUri.getHost());
-        LOGGER.info("port:" + redisUri.getPort());
-        LOGGER.info("userinfo:" + redisUri.getUserInfo());
-
         redis.setHostName(redisUri.getHost());
         redis.setPort(redisUri.getPort());
         redis.setPassword(redisUri.getUserInfo().split(":",2)[1]);
